8da859636d68452af879bb6590e9b4cb8ee3a470
Kyle Valade <kylevalade@gmail.com>
2014-03-21 00:38:50 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py-neorm4j",
+    version="0.0.1",
+
+    description='A basic ORM for Neo4j',
+    long_description=open('README.rst').read(),
+    author='Kyle Valade',
+    author_email='kylevalade@gmail.com',
+    keywords='python, osm, open streetmap, neo4j',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires=['neo4jrestclient==2.0.0'],
+    packages=find_packages(),
+    include_package_data=True,
+)
